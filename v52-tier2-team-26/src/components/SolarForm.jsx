a542c0becf2e5e753b24b68ad6188eb0628898df
Jordan J <jordanr2m@gmail.com>
2024-12-05 11:52:43 -0800

    Added success message for form submission.

--- v52-tier2-team-26/src/components/SolarForm.jsx
+++ v52-tier2-team-26/src/components/SolarForm.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import requests from "../data/requests";
+import { useNavigate } from "react-router-dom";
 
 const SolarForm = () => {
   const [requestsList, setRequestsList] = useState([requests]);
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const navigate = useNavigate();
   // Info gathered from inputs
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
@@ -11,10 +13,12 @@ const SolarForm = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
   const [date, setDate] = useState("");
   const [preferredTime, setPreferredTime] = useState("");
+  // TO DO: Add setAppointmentStatus to Context
+  const [appointmentStatus, setAppointmentStatus] = useState("Pending");
   // TO DO: Need an API call to verify address input
   const [address, setAddress] = useState("");
 
-  // Update local storage each time requestsLsit changes
+  // TO DO: Update local storage each time requestsLsit changes
 
   const addNewRequest = () => {
     const newRequest = {};
@@ -24,6 +28,7 @@ const SolarForm = () => {
     newRequest.phoneNumber = phoneNumber;
     newRequest.date = date;
     newRequest.time = preferredTime;
+    newRequest.status = appointmentStatus;
 
     // TO DO: Integrate address API
     newRequest.address = address;
@@ -31,99 +36,136 @@ const SolarForm = () => {
     setRequestsList([newRequest, ...requestsList]);
   };
   console.log(requestsList);
-  
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // Optional for smooth scrolling
+    });
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     addNewRequest();
     setIsSubmitted(true);
-  }
+    scrollToTop();
+  };
+
+  //   const clearFormFields = () => {
+  //     setFirstName("");
+  //     setLastName("");
+  //     setAddress("");
+  //     setEmail("");
+  //     setPhoneNumber("");
+  //     setDate("");
+  //     setPreferredTime("");
+  //   }
+
+  const cancelRequest = () => {
+    // clearFormFields();
+    navigate("/");
+    scrollToTop();
+  };
 
   return (
     <main className="SolarForm">
       <div className="wrapper form-wrapper">
-        <div className="form">
-          <div className="form-content">
-            <div className="heading-text">
-              <h2>You're eligible for a free LA Solar Panel Evaluation!</h2>
-              <p>Please complete the form below to get started.</p>
-            </div>
-            <form className="request-form" onSubmit={handleSubmit}>
-              <label htmlFor="fname">First Name</label>
-              <input
-                id="fname"
-                type="text"
-                required
-                onChange={(e) => setFirstName(e.target.value)}
-              />
-
-              <label htmlFor="lname">Last Name</label>
-              <input
-                id="lname"
-                type="text"
-                required
-                onChange={(e) => setLastName(e.target.value)}
-              />
-
-              <label htmlFor="address">Home Address</label>
-              <input
-                id="address"
-                type="text"
-                required
-                onChange={(e) => setAddress(e.target.value)}
-              />
-
-              <label htmlFor="email">Email Address</label>
-              <input
-                id="email"
-                type="email"
-                required
-                onChange={(e) => setEmail(e.target.value)}
-              />
-
-              <label htmlFor="phone">Phone Number</label>
-              <input
-                id="phone"
-                type="number"
-                required
-                onChange={(e) => setPhoneNumber(e.target.value)}
-              />
-
-              <h3>Choose Your Preferred Timeslot!</h3>
-              <label htmlFor="date">Date</label>
-              <input
-                id="date"
-                type="date"
-                required
-                onChange={(e) => setDate(e.target.value)}
-              />
-
-              <label htmlFor="timeslot">Timeslot</label>
-              <select
-                name="timeslot"
-                htmlFor="timeslot"
-                onChange={(e) => setPreferredTime(e.target.value)}
-              >
-                <option value="8AM-10AM">8 AM - 10 AM</option>
-                <option value="10AM-12PM">10 AM - 12 PM</option>
-                <option value="12PM-2PM">12 PM - 2 PM</option>
-                <option value="2PM-4PM">2 PM - 4 PM</option>
-              </select>
+        {isSubmitted ? (
+          <div className="submission-msg">
+            <div className="submission-msg-wrapper">
+              <h4>Thank you for submitting your request!</h4>
               <p>
-                *Please note that preferred timeslots are requested but not
-                garaunteed.
+                Please note that your preferred time slot has been requested,
+                but it is not garaunteed. You will receive an appointment
+                confirmation email before your scheduled visit.
               </p>
-
-              <button type="submit">Submit</button>
-              {/* TO DO: create function to set inputs blank & navigate to Home */}
-              <button className="cancel">Cancel Request</button>
-            </form>
+              <p>
+                <b>To cancel or make changes to your request</b>, please give us
+                a call at <a href="tel:+18001234567">1-800-123-4567</a>.
+              </p>
+            </div>
           </div>
-        </div>
+        ) : (
+          <div className="form">
+            <div className="form-content">
+              <div className="heading-text">
+                <h2>You're eligible for a free LA Solar Panel Evaluation!</h2>
+                <p>Please complete the form below to get started.</p>
+              </div>
+              <form className="request-form" onSubmit={handleSubmit}>
+                <label htmlFor="fname">First Name</label>
+                <input
+                  id="fname"
+                  type="text"
+                  required
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
 
-        {/* ADD popup Message Here! */}
-        <div className="success-message">
+                <label htmlFor="lname">Last Name</label>
+                <input
+                  id="lname"
+                  type="text"
+                  required
+                  onChange={(e) => setLastName(e.target.value)}
+                />
 
-        </div>
+                <label htmlFor="address">Home Address</label>
+                <input
+                  id="address"
+                  type="text"
+                  required
+                  onChange={(e) => setAddress(e.target.value)}
+                />
+
+                <label htmlFor="email">Email Address</label>
+                <input
+                  id="email"
+                  type="email"
+                  required
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+
+                <label htmlFor="phone">Phone Number</label>
+                <input
+                  id="phone"
+                  type="number"
+                  required
+                  onChange={(e) => setPhoneNumber(e.target.value)}
+                />
+
+                <h3>Choose Your Preferred Timeslot!</h3>
+                <label htmlFor="date">Date</label>
+                <input
+                  id="date"
+                  type="date"
+                  required
+                  onChange={(e) => setDate(e.target.value)}
+                />
+
+                <label htmlFor="timeslot">Timeslot</label>
+                <select
+                  name="timeslot"
+                  htmlFor="timeslot"
+                  onChange={(e) => setPreferredTime(e.target.value)}
+                >
+                  <option value="8AM-10AM">8 AM - 10 AM</option>
+                  <option value="10AM-12PM">10 AM - 12 PM</option>
+                  <option value="12PM-2PM">12 PM - 2 PM</option>
+                  <option value="2PM-4PM">2 PM - 4 PM</option>
+                </select>
+                <p>
+                  *Please note that preferred timeslots are requested but not
+                  garaunteed.
+                </p>
+
+                <button type="submit">Submit</button>
+                <button className="cancel" onClick={cancelRequest}>
+                  Cancel Request
+                </button>
+              </form>
+            </div>
+          </div>
+        )}
 
         <div className="form-imgs">
           <div className="building-img"></div>
