9284f3d3909c9257d8bafc1f8bd3b6f100d9e10b
chonger878 <echong06@gmail.com>
2024-12-08 11:59:34 -0800

    Added html/css for hero section

--- v52-tier2-team-26/src/components/Home.jsx
+++ v52-tier2-team-26/src/components/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import HowTo from "./HowTo";
+import HeroSection from "./HeroSection"
 
 const Home = () => {
   return (
     <main className="Home">
+      <HeroSection />
         <HowTo />
     </main>
   );
